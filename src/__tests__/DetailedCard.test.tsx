b26af7b51a0b5326bc294624805653c7d3417cd6
Space Pocket <space1985pocket@gmail.com>
2024-07-12 12:43:57 +0300

    refactor: update test for DetailedCard

--- src/__tests__/DetailedCard.test.tsx
+++ src/__tests__/DetailedCard.test.tsx
@@ -2,29 +2,27 @@ import { render, screen } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import '@testing-library/jest-dom';
 import { CardDetails } from '../components/cardDetails/CardDetails';
-import { responseData } from './mocs/responseData';
-import { PotterDbApi } from '../service/potterDbApi';
-import { PotterDbApiReturnType } from '../types';
+import { responseDataForChar } from './mocs/responseData';
 
 describe('Tests for the CardList component', () => {
   it('Make sure the detailed card component correctly displays the detailed card data', async () => {
-    const mockCharacter = responseData.data[0];
-
-    vitest.mock('../../service/potterDbApi', () => ({
-      PotterDbApi: vitest.fn(),
+    vitest.mock('../service/potterDbApi', () => ({
+      PotterDbApi: vitest.fn(() => ({
+        getCharacter: vitest.fn().mockResolvedValue(responseDataForChar),
+        getCharacters: vitest.fn(),
+        loading: false,
+        error: '',
+        clearError: vitest.fn(),
+        _DefaultFilterWord: '',
+        _DefaultOffset: '',
+        _DefaultPage: 1,
+      })),
     }));
-    const mockPotterDbApi: PotterDbApiReturnType = PotterDbApi();
-    mockPotterDbApi.getCharacter = vitest
-      .fn()
-      .mockResolvedValue({ data: mockCharacter });
-    mockPotterDbApi.error = '';
-    mockPotterDbApi.loading = false;
 
-    vitest.mock('../../service/potterDbApi', () => mockPotterDbApi);
     render(
       <Router>
         <CardDetails
-          characterId={mockCharacter.id}
+          characterId={responseDataForChar.data.id}
           onHideCardDetails={function (): void {}}
           cardDetails={'0'}
         />
