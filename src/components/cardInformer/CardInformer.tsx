03d86da7916d8d63c0904978f701a5452dfd9b16
Space Pocket <space1985@pocket@gmail.com>
2024-07-21 22:00:26 +0300

    feat: implemented download option

--- src/components/cardInformer/CardInformer.tsx
+++ src/components/cardInformer/CardInformer.tsx
@@ -1,14 +1,14 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { useAppSelector, useAppDispatch } from '../../hooks/storeHooks';
 import {
   removeAllSelectedChars,
   unSelectAllCharacters,
 } from '../../store/slices/charactersSlice';
 import styles from './CardInformer.module.css';
-import InformerImg from '../../assets/informer.png'
+import InformerImg from '../../assets/informer.png';
 export const CardInformer = (): JSX.Element => {
-  const countSelectedCards = useAppSelector(
-    (state) => state.characters.selectedChacharacters.length
+  const selectedCards = useAppSelector(
+    (state) => state.characters.selectedChacharacters
   );
 
   const [isInformerAnimated, setIsInformerAnimated] = useState(false);
@@ -18,7 +18,7 @@ export const CardInformer = (): JSX.Element => {
   }`;
 
   useEffect(() => {
-    if (countSelectedCards === 0) {
+    if (selectedCards.length === 0) {
       return;
     }
     setIsInformerAnimated(true);
@@ -30,11 +30,56 @@ export const CardInformer = (): JSX.Element => {
     return () => {
       clearTimeout(timer);
     };
-  }, [countSelectedCards]);
+  }, [selectedCards]);
 
   const handlerUnselectAll = () => {
     dispatch(removeAllSelectedChars());
     dispatch(unSelectAllCharacters());
+  };
+
+  const downloadLinkRef = useRef<HTMLAnchorElement | null>(null);
+
+  const convertToCSV = () => {
+    const header = [
+      'Name',
+      'Gender',
+      'Born',
+      'Blood status',
+      'Eye color',
+      'Wiki',
+    ];
+    const rows = selectedCards.map((item) => [
+      item.attributes.name,
+      item.attributes.gender,
+      item.attributes.born,
+      item.attributes.blood_status,
+      item.attributes.eye_color,
+      item.attributes.wiki,
+    ]);
+
+    let csvContent = header.join(',') + '\n';
+    rows.forEach((row) => {
+      csvContent += row.join(',') + '\n';
+    });
+
+    return csvContent;
+  };
+
+  const handleDownload = () => {
+    const csvContent = convertToCSV();
+    const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+    const url = URL.createObjectURL(blob);
+
+    const downloadLink = downloadLinkRef.current;
+    const filename = `${selectedCards.length}_characters.csv`;
+
+    if (downloadLink) {
+      downloadLink.href = url;
+      downloadLink.download = filename;
+      downloadLink.click();
+    }
+
+    URL.revokeObjectURL(url);
   };
 
   const content = (
@@ -42,13 +87,14 @@ export const CardInformer = (): JSX.Element => {
       <div className={informerClass}>
         <div className={styles.informerLIne}>
           <img src={InformerImg} alt="" />
-          <span>{`Selected - ${countSelectedCards} cards`}</span>
+          <span>{`Selected - ${selectedCards.length} cards`}</span>
         </div>
       </div>
       <button onClick={handlerUnselectAll}>Unselect all</button>
-      <button>Download</button>
+      <a ref={downloadLinkRef} style={{ display: 'none' }}></a>
+      <button onClick={handleDownload}>Download</button>
     </div>
   );
 
-  return <>{countSelectedCards > 0 && content}</>;
+  return <>{selectedCards.length > 0 && content}</>;
 };
