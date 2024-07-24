6108005fac609b3a854b573da13cb5f54d591644
Space Pocket <space1985pocket@gmail.com>
2024-07-24 08:37:29 +0300

    feat: add CharactersSlice.test

--- src/store/slices/charactersSlice.ts
+++ src/store/slices/charactersSlice.ts
@@ -1,7 +1,7 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { TransformCharacterType } from '../../types';
 
-type CharacterstStateType = {
+export type CharacterstStateType = {
   characterList: Array<TransformCharacterType>;
   selectedChacharacters: Array<TransformCharacterType>;
 };
